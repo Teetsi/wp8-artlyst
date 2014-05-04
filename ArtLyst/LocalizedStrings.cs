cd38c53d14be78064059f29e6f18e1d2ffaa24ac
Viktor Kelemen <kelemen.viktor@gmail.com>
2014-05-04 21:58:14 +0100

    Init repo

---
+++ ArtLyst/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ArtLyst.Resources;
+
+namespace ArtLyst
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
